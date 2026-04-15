32dde29db6816bff559fc65af72bf84bf6ddcc13
Null <null3hub3@gmail.com>
2026-04-15 18:27:07 -0300

    ]

---
+++ bot/utils/channelManager.js
@@ -0,0 +1,21 @@
+const config = require('../config/ticketConfig.json');
+
+async function createTicketChannel(guild, user) {
+  const category = guild.channels.cache.find(c => c.name === config.categoryName) ||
+    await guild.channels.create({ name: config.categoryName, type: 0 });
+
+  const name = config.channelNameFormat.replace('{username}', user.username.toLowerCase());
+  
+  return guild.channels.create({
+    name,
+    type: 0,
+    parent: category,
+    permissionOverwrites: [
+      { id: guild.roles.everyone, deny: ['ViewChannel'] },
+      { id: user.id, allow: ['ViewChannel', 'SendMessages', 'ReadMessageHistory'] },
+      { id: config.supportRoleId, allow: ['ViewChannel', 'SendMessages', 'ReadMessageHistory', 'ManageChannels'] }
+    ]
+  });
+}
+
+module.exports = { createTicketChannel };
